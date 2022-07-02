7245f1050c62672cab1edadc861df76c2153607c
Queeniffy <fikumi143@gmail.com>
2022-07-01 17:03:50 -0700

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - Prints out the last part of a quote in the standard error.
+* main - Entry point
 *
-* Return: 0 if success.
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-Char str1[] = "and that piece of art is useful\" - Dora Korpar,  2015-10-19\n";
-write(2, str1, 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
