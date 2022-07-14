276915f2372b5294f9be96d81043a388c0df7693
Queeniffy <fikumi143@gmail.com>
2022-07-14 00:04:41 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 */
 int main(void)
 {
-
-	char joy[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i = i + 1)
-	{
-		_putchar(joy[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
-}	
+}
