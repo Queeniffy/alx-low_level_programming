5435fa2236822ae348ac30a490d51c1b048cd40b
Queeniffy <fikumi143@gmail.com>
2022-07-23 03:15:31 -0700

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
 /**
-*main - Entry point
-*
-*Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	int i = 0;
+	char text[] = "_putchar";
+
+	while (text[i] != '\0')
+	{
+		_putchar(text[i]);
+		i++;
+	}
 	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-loops
+wow
